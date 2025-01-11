2f5c58a6612a62ca591c4fe36260598da3b7b642
Patryk Kowalczyk <papyszoo@gmail.com>
2025-01-11 14:48:07 +0100

    adjusted camera position

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import Box from "./Box";
 function App() {
     return (
         <>
-            <Canvas>
+            <Canvas camera={{ position: [0.35, 0.6, 1] }}>
                 <color attach="background" args={["skyblue"]} />
-                {/* <ambientLight intensity={0.5} /> */}
-                <OrbitControls />
+                <ambientLight intensity={0.5} />
+                <OrbitControls
+                    minPolarAngle={0}
+                    maxPolarAngle={Math.PI / 1.9}
+                    makeDefault
+                />
                 <Stage
                     preset="rembrandt"
                     adjustCamera={false}
