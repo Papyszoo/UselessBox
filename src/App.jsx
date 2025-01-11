be8a3b69c6c9b6f0b15bde1243e6e8387337cb90
Patryk Kowalczyk <papyszoo@gmail.com>
2025-01-11 14:37:08 +0100

    Added materials

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ function App() {
     return (
         <>
             <Canvas>
+                <color attach="background" args={["skyblue"]} />
+                {/* <ambientLight intensity={0.5} /> */}
                 <OrbitControls />
-                <Stage>
-                    <Box />
+                <Stage
+                    preset="rembrandt"
+                    adjustCamera={false}
+                    center={false}
+                    position-y={-0.01}
+                    intensity={0.5}
+                >
+                    <Box scale={0.75} rotation-y={-Math.PI} />
                 </Stage>
             </Canvas>
         </>
--- src/Box.jsx
+++ src/Box.jsx
@@ -4,77 +4,114 @@ import * as THREE from "three";
 
 const Box = (props) => {
     const group = useRef();
-    const { nodes, materials, animations } = useGLTF("/box.glb");
+    const { nodes, animations } = useGLTF("/box.glb");
     const { actions, mixer } = useAnimations(animations, group);
 
     const turnOn = () => {
-        console.log("test");
-        const turnOnAction = mixer.clipAction(animations[1]);
-        const turnOffAction = mixer.clipAction(animations[0]);
-        turnOnAction.setLoop(THREE.LoopOnce);
-        turnOnAction.clampWhenFinished = true;
-        turnOnAction.play();
+        if (
+            actions.TurnOn.isRunning() ||
+            (actions.TurnOff.isRunning() && actions.TurnOff.time < 2)
+        ) {
+            return;
+        }
+        actions.TurnOn.setLoop(THREE.LoopOnce);
+        actions.TurnOn.clampWhenFinished = true;
+        actions.TurnOn.play();
         mixer.addEventListener("finished", () => {
-            turnOnAction.stop();
-            turnOffAction.setLoop(THREE.LoopOnce);
-            turnOffAction.clampWhenFinished = true;
-            turnOffAction.play();
-            mixer.addEventListener("finished", () => {
-                turnOffAction.stop();
-                turnOffAction.fadeOut();
-                turnOffAction.reset();
-                turnOnAction.reset();
-            });
+            turnOnFinished();
         });
+    };
+
+    const turnOnFinished = () => {
+        actions.TurnOn.stop();
+        actions.TurnOn.reset();
+        if (actions.TurnOff.isRunning()) {
+        }
+        actions.TurnOff.setLoop(THREE.LoopOnce);
+        actions.TurnOff.clampWhenFinished = true;
+        actions.TurnOff.play();
+        mixer.addEventListener("finished", () => {
+            turnOffFinished();
+        });
+    };
+
+    const turnOffFinished = () => {
+        actions.TurnOff.stop();
+        actions.TurnOff.fadeOut();
+        actions.TurnOff.reset();
     };
 
     return (
         <group ref={group} {...props} dispose={null}>
-            <group name="Scene">
-                <mesh
-                    name="Box"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Box.geometry}
-                    material={nodes.Box.material}
-                    position={[-0.252, 0.329, -0.007]}
+            <mesh
+                name="Box"
+                castShadow
+                receiveShadow
+                geometry={nodes.Box.geometry}
+                material={nodes.Box.material}
+                position={[-0.252, 0.329, -0.007]}
+            >
+                <meshStandardMaterial
+                    color="black"
+                    side={THREE.DoubleSide}
+                    roughness={0.35}
                 />
-                <mesh
-                    name="Latch"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Latch.geometry}
-                    material={nodes.Latch.material}
-                    position={[1, 0.461, -0.001]}
+            </mesh>
+            <mesh
+                name="Latch"
+                castShadow
+                receiveShadow
+                geometry={nodes.Latch.geometry}
+                material={nodes.Latch.material}
+                position={[1, 0.461, -0.001]}
+            >
+                <meshStandardMaterial color="black" roughness={0.35} />
+            </mesh>
+            <mesh
+                name="Screw"
+                castShadow
+                receiveShadow
+                geometry={nodes.Screw.geometry}
+                material={nodes.Screw.material}
+                position={[-0.136, 0.476, 0]}
+            >
+                <meshStandardMaterial
+                    color="white"
+                    metalness={1}
+                    roughness={0}
                 />
-                <mesh
-                    name="Screw"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Screw.geometry}
-                    material={nodes.Screw.material}
-                    position={[-0.136, 0.476, 0]}
+            </mesh>
+            <mesh
+                name="Circle"
+                castShadow
+                receiveShadow
+                geometry={nodes.Circle.geometry}
+                material={nodes.Circle.material}
+                position={[-0.115, 0.356, 0]}
+                rotation={[0, 0, -1.082]}
+            >
+                <meshStandardMaterial
+                    color="white"
+                    metalness={1}
+                    roughness={0}
                 />
-                <mesh
-                    name="Circle"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Circle.geometry}
-                    material={nodes.Circle.material}
-                    position={[-0.115, 0.356, 0]}
-                    rotation={[0, 0, -1.082]}
+            </mesh>
+            <mesh
+                name="Switch"
+                castShadow
+                receiveShadow
+                geometry={nodes.Switch.geometry}
+                material={nodes.Switch.material}
+                position={[-0.136, 0.461, 0]}
+                rotation={[0, 0, 0.209]}
+                onClick={turnOn}
+            >
+                <meshStandardMaterial
+                    color="white"
+                    metalness={1}
+                    roughness={0}
                 />
-                <mesh
-                    name="Switch"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Switch.geometry}
-                    material={nodes.Switch.material}
-                    position={[-0.136, 0.461, 0]}
-                    rotation={[0, 0, 0.209]}
-                    onClick={turnOn}
-                />
-            </group>
+            </mesh>
         </group>
     );
 };
